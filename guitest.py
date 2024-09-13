5a73cb84a84bed229b8e2239c72135749c9e5207
paultribble <ptribble@uark.edu>
2024-09-13 14:17:24 -0500

    Commit

--- guitest.py
+++ guitest.py
@@ -23,6 +23,27 @@ red = (255, 0, 0)
 # Fonts
 font = pygame.font.Font(None, 36)
 
+class Button:
+    def __init__(self, x, y, w, h, text, callback):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = gray
+        self.text = text
+        self.font = pygame.font.Font(None, 36)
+        self.txt_surface = self.font.render(text, True, black)
+        self.callback = callback
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.callback()
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        screen.blit(self.txt_surface, (self.rect.x + (self.rect.w - self.txt_surface.get_width()) // 2,
+                                       self.rect.y + (self.rect.h - self.txt_surface.get_height()) // 2))
+
+
+
 # Load logo image
 def load_image(filename):
     script_dir = os.path.dirname(os.path.abspath(__file__))
