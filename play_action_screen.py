c61e12d53ae99ca767f10b7a81a1f3e9033a0a6b
brookeavant <167897916+brookeavant@users.noreply.github.com>
2024-10-18 20:15:53 -0500

    Greer

--- play_action_screen.py
+++ play_action_screen.py
@@ -157,7 +157,7 @@ class PlayActionScreen:
             self.master.geometry("800x600")
 
             #Bind the F5 key to the startgame method
-            self.master.bind('<F5>', self.startgame)
+            self.master.bind('<s>', self.startgame)
 
         def startgame(self, event):
             broad_castport = 12345
