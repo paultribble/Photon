d91837498edfa6a31913cf42be5cdb13baad2973
brookeavant <167897916+brookeavant@users.noreply.github.com>
2024-11-17 19:35:07 -0600

    nonlocal setup_screen

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-#main.py
-
 import tkinter as tk
 from database import Database
 from udp_communication import UDPCommunication
 from splash_screen import SplashScreen
 from setup_screen import SetupScreen
-from play_action_screen import PlayActionScreen
 import atexit
-from pynput import keyboard
 
 def main():
     # Initialize the main Tkinter window
     root = tk.Tk()
     root.withdraw()  # Hide the main window initially
 
-        
     # Initialize database connection
     database = Database()
-
 
     # Initialize UDP Communication
     try:
@@ -25,7 +19,7 @@ def main():
     except OSError:
         print("Failed to bind UDP ports. Make sure no other application is using these ports.")
         return
-    
+
     # Show splash screen
     splash = SplashScreen(root, "Images/logo.jpg", duration=3000)
 
@@ -34,28 +28,24 @@ def main():
 
     # After splash screen, show setup screen
     def show_setup_screen():
+        nonlocal setup_screen  # Access the outer scope variable
         root.deiconify()  # Show the main window
         setup_screen = SetupScreen(root, database, udp_comm)
 
     root.after(3000, show_setup_screen)  # Schedule to show setup screen after splash
-
-    # Function to start the game (open Play Action Screen)
-    #def start_game(red_team, blue_team):
-        #udp_comm.send_broadcast("202")  # Send the 202 broadcast message
-       # PlayActionScreen(root, udp_comm, red_team, blue_team)
 
     # Handle application exit to ensure sockets are closed
     def on_close():
         print("on_close triggered")
         nonlocal setup_screen  # Access the outer scope variable
         if setup_screen:  # Check if setup_screen has been initialized
-            setup_screen.stop_music() 
+            print("Stopping music...")
+            setup_screen.stop_music()
         udp_comm.close_sockets()
         database.close()
         root.destroy()
 
     root.protocol("WM_DELETE_WINDOW", on_close)
-
 
     # Register cleanup in case of unexpected exits
     atexit.register(on_close)
